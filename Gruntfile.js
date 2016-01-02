25989a6cdb166d99a059bd8aa4cce61d1782b332
JesusGuerrero <rocha.jesus@hotmail.com>
2016-01-01 22:25:39 -0800

    site - added parallax + easing + about page + images

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,9 +65,11 @@ module.exports = function (grunt) {
       bower: {
         src: [
           'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/jquery-ui/jquery-ui.js',
           'bower_components/bootstrap/dist/js/bootstrap.min.js',
           'bower_components/OwlCarousel2/dist/owl.carousel.min.js',
-          'bower_components/classie/classie.js'
+          'bower_components/classie/classie.js',
+          'bower_components/jquery.parallax/jquery.parallax.js'
         ],
         dest: 'server/public/js/bower.js'
       },
---
+++ server/public/js/components/about/team.js
@@ -0,0 +1,24 @@
+$(window).load( function(){
+  $('.parallaxBg').parallax("50%", 0.2);
+});
+$(document).ready( function(){
+  $('.team-profiles .profile').hover(
+    function() {
+      console.log('in here');
+      $(this).find('.caption').stop().animate({
+        bottom: 0
+      }, 200, 'easeOutQuad');
+      $(this).find('img').stop().animate({
+        top: -20
+      }, 300, 'easeOutQuad');
+    },
+    function() {
+      $(this).find('.caption').stop().animate({
+        bottom: -75
+      }, 200, 'easeInQuad');
+      $(this).find('img').stop().animate({
+        top: 0
+      }, 300, 'easeOutQuad');
+    }
+  );
+});
